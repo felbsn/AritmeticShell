6c7c044fa894d1b8eb3e2e9808c09447dcdc2cc0
felbsn <felbsn@gmail.com>
2018-05-29 01:15:04 +0300

    Project added from CVector examles

---
+++ main.c
@@ -0,0 +1,18 @@
+#pragma warning(disable:4996)
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "Examples/FunctionSolver.h"
+
+int main(int argc, char ** argv)
+{
+
+
+	while (1)
+	{
+
+		ParseFunction();
+		system("pause");
+	}
+}
+
